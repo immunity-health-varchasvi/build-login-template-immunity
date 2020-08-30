403cfbea004e8c6d2f6210c078c16843a45eb1ca
varchasvipandey <varchasvip@gmail.com>
2020-08-31 00:36:05 +0530

    build with specific link

---
+++ precache-manifest.3a1b822fe1550623f21aeb5fc03b21a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f70e2be8727fe02b364762d6c9d29b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c8c053bdb3caee61437",
+    "url": "/static/css/2.75056432.chunk.css"
+  },
+  {
+    "revision": "fbfd07c2cff700d6defd",
+    "url": "/static/css/main.409b6420.chunk.css"
+  },
+  {
+    "revision": "2c8c053bdb3caee61437",
+    "url": "/static/js/2.c147d60e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c147d60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbfd07c2cff700d6defd",
+    "url": "/static/js/main.84abd096.chunk.js"
+  },
+  {
+    "revision": "aeb2c35c26ea2c2fb129",
+    "url": "/static/js/runtime-main.5b34f156.js"
+  },
+  {
+    "revision": "8d1a884f40d0944bfaca7fcb82cbcb43",
+    "url": "/static/media/logo.8d1a884f.png"
+  }
+]);
